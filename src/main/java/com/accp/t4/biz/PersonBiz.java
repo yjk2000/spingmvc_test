db015c4f16e5562ef712f775177398b1da9f718d
yjk2000 <2410654492@qq.com>
2020-02-21 15:39:46 +0800

    谁谁谁

--- src/main/java/com/accp/t4/biz/PersonBiz.java
+++ src/main/java/com/accp/t4/biz/PersonBiz.java
@@ -42,6 +42,7 @@ public class PersonBiz {
 	public void addPerson(Person person) {
 		System.out.println("第一次修改");
 		System.out.println("第二次修改");
+		System.out.println("第三次修改");
 		personDao.savePerson(person);
 	}
 
