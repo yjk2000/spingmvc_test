e9a5aea940191a8e7e261dd223503087ff274ef2
yjk2000 <2410654492@qq.com>
2020-02-21 15:43:49 +0800

    第4次

--- src/main/java/com/accp/t4/biz/PersonBiz.java
+++ src/main/java/com/accp/t4/biz/PersonBiz.java
@@ -43,6 +43,7 @@ public class PersonBiz {
 		System.out.println("第一次修改");
 		System.out.println("第二次修改");
 		System.out.println("第三次修改");
+		System.out.println("第四次修改");
 		personDao.savePerson(person);
 	}
 
